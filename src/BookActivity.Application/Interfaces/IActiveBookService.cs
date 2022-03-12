7476dbe6edb050de6f3dc25aa9d13060e3b02a40
Antanidoss <antosci2002@gmail.com>
2022-03-12 22:18:51 +0300

    Added active book Interface

---
+++ src/BookActivity.Application/Interfaces/IActiveBookService.cs
@@ -0,0 +1,17 @@
+﻿using BookActivity.Application.Models.DTO;
+using BookActivity.Application.Models.Filters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookActivity.Application.Interfaces
+{
+    public interface IActiveBookService : IBaseService<ActiveBookDTO, ActiveBookFilterModel>
+    {
+       
+    }
+}
