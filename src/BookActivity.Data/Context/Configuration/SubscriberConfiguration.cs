a6b384619c033ec92409435fa1d21908b2500505
Antanidoss <antosci2002@gmail.com>
2023-01-16 19:38:48 +0300

    Added complex key for subscriber and subscription

---
+++ src/BookActivity.Data/Context/Configuration/SubscriberConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using BookActivity.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookActivity.Infrastructure.Data.Context.Configuration
+{
+    internal sealed class SubscriberConfiguration : IEntityTypeConfiguration<Subscriber>
+    {
+        public void Configure(EntityTypeBuilder<Subscriber> builder)
+        {
+            builder.Ignore(s => s.Id)
+                .HasKey(s => new { s.SubscribedUserId, s.UserIdWhoSubscribed });
+        }
+    }
+}
--- src/BookActivity.Shared/Utils/HttpContextDataGetter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace BookActivity.Shared.Utils
-{
-    public static class HttpContextDataGetter
-    {
-        public static string GetConnectionId(IHttpContextAccessor httpContextAccessor) => httpContextAccessor.HttpContext.Request.Headers["ConnectionId"];
-    }
-}
