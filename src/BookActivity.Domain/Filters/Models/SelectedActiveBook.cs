2f748987823d95d146c0ca882cc0424dc732923d
Antanidoss <antosci2002@gmail.com>
2023-02-25 09:14:59 +0300

    Added info about rating and opinion when get activeBook

--- src/BookActivity.Domain/Filters/Models/SelectedActiveBook.cs
+++ src/BookActivity.Domain/Filters/Models/SelectedActiveBook.cs
@@ -12,5 +12,14 @@ namespace BookActivity.Domain.Filters.Models
         public string BookTitle { get; set; }
         public byte[] ImageData { get; set; }
         public IEnumerable<SelectedBookNote> BookNotes { get; set; }
+        public Guid BookRatingId { get; set; }
+        public SelectedBookOpinion BookOpinion { get; set; }
+
+        public class SelectedBookOpinion
+        {
+            public Guid Id { get; set; }
+            public float Grade { get; set; }
+            public string Description { get; set; }
+        }
     }
 }
