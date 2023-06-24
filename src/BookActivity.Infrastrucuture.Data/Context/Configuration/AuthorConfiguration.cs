99f9c59efb8c675061c6f9ffac9f9ee15d1dab90
Antanidoss <antosci2002@gmail.com>
2023-06-24 19:17:23 +0300

    Added base entity configuration

---
+++ src/BookActivity.Infrastrucuture.Data/Context/Configuration/AuthorConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using BookActivity.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookActivity.Infrastructure.Data.Context.Configuration
+{
+    internal sealed class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.Property(a => a.FirstName).HasMaxLength(50).IsRequired();
+            builder.Property(a => a.Surname).HasMaxLength(50).IsRequired();
+        }
+    }
+}
---
+++ src/BookActivity.Infrastrucuture.Data/Context/Configuration/BookNoteConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using BookActivity.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookActivity.Infrastructure.Data.Context.Configuration
+{
+    internal sealed class BookNoteConfiguration : IEntityTypeConfiguration<BookNote>
+    {
+        public void Configure(EntityTypeBuilder<BookNote> builder)
+        {
+            builder.Property(n => n.Note).HasMaxLength(300).IsRequired();
+        }
+    }
+}
