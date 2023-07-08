59661111ef3a40b5f61c6ea162e208ecb54aeb46
Antanidoss <antosci2002@gmail.com>
2023-07-08 12:28:24 +0300

    Name property refactoring

--- src/BookActivity.Domain/Queries/ActiveBookStatisticQueries/ActiveBooksStatistic.cs
+++ src/BookActivity.Domain/Queries/ActiveBookStatisticQueries/ActiveBooksStatistic.cs
@@ -5,10 +5,16 @@ namespace BookActivity.Domain.Queries.ActiveBookStatisticQueries
 {
     public sealed class ActiveBooksStatistic
     {
-        public IEnumerable<(int CountPagesRead, DateTime Date)> NumberOfPagesReadPerDay { get; set; }
+        public IEnumerable<NumberOfPagesReadPerDay> ReadingCalendar { get; set; }
         public float AveragePagesReadPerDay { get; set; }
         public float AveragePagesReadPerWeek { get; set; }
         public float AveragePagesReadPerMouth { get; set; }
         public int AmountDaysOfReads { get; set; }
+    }
+
+    public class NumberOfPagesReadPerDay
+    {
+        public int CountPagesRead { get; set; }
+        public string Date { get; set; }
     }
 }
